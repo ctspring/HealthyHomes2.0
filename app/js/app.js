111e6b6974f40e1b1f26686c968a774722278003
Gaby Cruz <gcruz.a10@gmail.com>
2014-06-12 12:39:27 -0500

    implemented the nested pages in the beatles.html.  need to put home button on each page (probably in a <div>).

--- app/js/app.js
+++ app/js/app.js
@@ -3,21 +3,32 @@
 /* App Module */
 
 var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute',
-  'phonecatControllers', 'ngAnimate'
-]);
+  'ngRoute', 'phonecatControllers', 'ngAnimate'
+]); //dependencies
 
 phonecatApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/phones', {
-        templateUrl: 'partials/phone-list.html',
+        templateUrl: 'partials/loginPage.html',
         controller: 'PhoneListCtrl'
       }).
       when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
+        templateUrl: 'partials/beatles.html',
+        controller: 'PhoneListCtrl'
       }).
+      when('/clickme1', {
+        templateUrl: 'partials/clickMe1.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/clickme2', {
+        templateUrl: 'partials/clickMe2.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/clickme3', {
+        templateUrl: 'partials/clickMe3.html',
+        controller: 'PhoneListCtrl'
+      }).     
       otherwise({
         redirectTo: '/phones'
       });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,21 +3,20 @@
 /* Controllers */
 
 var phonecatControllers = angular.module('phonecatControllers', []);
-
 phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
   function($scope, $http) {
     $http.get('phones/phones.json').success(function(data) {
       $scope.phones = data;
     });
-
-    $scope.orderProp = 'age';
   }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+/*
+phonecatControllers.controller('beatlesCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
     $scope.phoneId = $routeParams.phoneId;
   }]);
-
+*/
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 
