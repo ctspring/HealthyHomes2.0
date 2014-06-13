2043afe831203d990ecb1df44605ec0cb3a3a229
Gaby Cruz <gcruz.a10@gmail.com>
2014-06-12 21:55:12 -0500

    almost done implementing the "talking" to database.  not sure where the problem is though.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -17,9 +17,37 @@ phonecatControllers.controller('beatlesCtrl', ['$scope', '$routeParams',
     $scope.phoneId = $routeParams.phoneId;
   }]);
 */
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 
 function myCtrl($scope){
     $scope.songs=['Sgt. Peppers Lonely Hearts Club Band','With a Little Help from My Friends','Lucy in the Sky with Diamonds','Getting Better' ,'Fixing a Hole','Shes Leaving Home','Being for the Benefit of Mr. Kite!' ,'Within You Without You','When Im Sixty-Four','Lovely Rita','Good Morning Good Morning','Sgt. Peppers Lonely Hearts Club Band (Reprise)','A Day in the Life'];
   }
+
+// %%%%%%%%%% to talk to DB %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+var databaseController = angular.module('databaseControllerModule', []);
+databaseController.controller('dbDisplayController', ['$scope', 'databaseConnection',
+  function($scope, databaseConnection) {
+    $scope.hello = databaseConnection.hello(); //result to hello method, technically same as query
+    $scope.queryResult = databaseConnection.query(); //result to query
+      
+    //Using the login() function without callback
+    //$scope.login = databaseConnection.login({"data": {"userName":"user", "passWord": "pass"}});
+    
+    databaseConnection.login({"data": {userName:"user", passWord: "pass"}}, function(value){
+        //This is a call back function for the login() action on the resource object
+        //console.log(value);
+        $scope.login = value;
+        $scope.loginData = JSON.parse(value.data);
+    });
+    
+    $scope.send = function(){
+        $scope.user = databaseConnection.queryWebService({"queryA":$scope.queryA});
+        $scope.pass = databaseConnection.queryWebService({"query1":$scope.query1});        
+        $scope.inputText='';
+    };
+      
+  }]);
+
+
